cffc52839f5e1ba3a360f06346ca836d18c3f7e1
Mohammedayman0 <159598291+Mohammedayman0@users.noreply.github.com>
2026-04-28 23:40:15 +0300

    Add files via upload

--- app_FINAL_FIXED.py
+++ app_FINAL_FIXED.py
@@ -810,7 +810,7 @@ elif step == 3:
                 الكود <code>GRP-001</code> يُستخدم في الدمج.
             </div>
             """, unsafe_allow_html=True)
-            show_limit = safe_slider("عدد المجموعات:", len(clusters)), min(20, len(clusters), key="cl_limit")
+            show_limit = safe_slider("عدد المجموعات المعروضة:", len(clusters), key="cl_limit")
             for i, cluster in enumerate(clusters[:show_limit]):
                 golden = pick_golden_record(cluster["names"])
                 sc_val = cluster["max_score"]
@@ -931,7 +931,7 @@ elif step == 4:
     if "golden_overrides" not in st.session_state:
         st.session_state.golden_overrides = {}
 
-    show_n = safe_slider("عدد المجموعات:", len(clusters)), min(20, len(clusters)), key="merge_limit")
+    show_n = safe_slider("عدد المجموعات:", len(clusters), key="merge_limit")
 
     for i, cluster in enumerate(clusters[:show_n]):
         auto_golden = pick_golden_record(cluster["names"])
